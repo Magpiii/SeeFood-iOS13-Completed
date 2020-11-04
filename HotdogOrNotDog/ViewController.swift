06c80965163903853b18fddbe4c948f85986859e
Hunter Hudson <hunterhudson3@gmail.com>
2020-11-04 12:03:58 -0700

    Initial Commit

---
+++ HotdogOrNotDog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotdogOrNotDog
+//
+//  Created by Hunter Hudson on 11/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
